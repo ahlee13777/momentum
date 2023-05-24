24af56d41ea9dbb45358e193f31d8b3eec04db2a
ahlee13777 <ahlee13777@gmail.com>
2023-05-24 20:11:20 +0900

    Fix : 시계 1초대 형식 수정
    00:00:1로 표기되는 것을 00:00:01로 표기되게 수정하였음
    
    fixed #0

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,17 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock(){
-    const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const date = new Date(); //js 내장 객체
+    const hours = String(date.getHours()).padStart(2, "0"); //date 객체에서 매서드 사용 
+    const minutes = String(date.getMinutes()).padStart(2, "0"); //padStart(최대길이, 채울거)
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    //clock.innerText = `${date.getHoursdate.getMinutes()}:${date.getSeconds()}`;
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
 setInterval(getClock,1000); //(함수,interval(ms)) 반복실행
-//setTimeout(say,Hello, 5000) //1번실행
+//setTimeout(say,Hello, 5000) //1번실행ls
 
-//1의자리 단위 수정해야됨
+
