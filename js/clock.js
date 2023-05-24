dd40c2f12c048d85fee44b4acc138db937abad4e
ahlee13777 <ahlee13777@gmail.com>
2023-05-24 21:17:17 +0900

    feat : 랜덤 명언 띄우기
    랜덤하게 명언과 작가를 보여주는 기능
    
    fix #1

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,8 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock(){
-    const date = new Date(); //js 내장 객체
+    const date = new Date(); //js 내장 객체 Date오브젝트를 만듦
+    
     const hours = String(date.getHours()).padStart(2, "0"); //date 객체에서 매서드 사용 
     const minutes = String(date.getMinutes()).padStart(2, "0"); //padStart(최대길이, 채울거)
     const seconds = String(date.getSeconds()).padStart(2, "0");
